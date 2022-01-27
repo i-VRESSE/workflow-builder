a00ff689c03101241af4637334be7f5bfdcdc1f9
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-01-27 15:59:14 +0100

    More todos

--- src/TextPanel.tsx
+++ src/TextPanel.tsx
@@ -7,14 +7,16 @@ import { useText } from './store'
 
 SyntaxHighlighter.registerLanguage('toml', toml)
 
-export const TextPanel = () => {
+export const TextPanel = (): JSX.Element => {
   const code = useText()
 
-  async function copy2clipboard () {
+  async function copy2clipboard (): Promise<void> {
     await navigator.permissions.query({ name: 'clipboard-write' } as any)
     await navigator.clipboard.writeText(code)
   }
 
+  // TODO would be nice if text was editable and showed parameter description on hover and inline validation errors.
+  // TODO would be nice to be able to click in text to select step to edit.
   return (
     <div>
       <SyntaxHighlighter language='toml' style={style}>
