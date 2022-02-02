2170fad4ec59c6d5b6b0f13213a6c7f30fa3d79e
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2022-02-02 14:21:54 +0100

    Process defaults.yaml from haddock3 to generate catalogs (#22)
    * Process defaults.yaml from haddock3 to generate catalogs
    
    * Now with filled catalog
    
    * forget default when it is nan. Form can not handle nans
    
    * A parameter which is an object is stored as a section in toml
    
    * Add back run_dir
    
    * More todos
    
    * Undo grouping on catalog schema level
    
    * Dont sort keys in generated catalog, use same order as in source file
    
    * Use maxitems/minitems for list
    
    * Store group in uiSchema
    
    * Use expert levels from haddock3 + describe translations from haddock3 to workflow builder

--- util/generate_haddock3_catalog.py
+++ util/generate_haddock3_catalog.py
@@ -4,28 +4,55 @@ Requires haddock3, fcc and pyyaml
 
 Run with
 
-```
+```shell
 util/generate_haddock3_catalog.py --level basic public/haddock3.basic.catalog.yaml
 util/generate_haddock3_catalog.py --level intermediate public/haddock3.intermediate.catalog.yaml
 util/generate_haddock3_catalog.py --level guru public/haddock3.guru.catalog.yaml
 ```
+
+Translations from haddock3 -> i-VRESSE workflow builder:
+
+* module -> node
+* generic parameters-> global parameters
+* Module info
+    * haddock.modules.<catagory>.<module>.__doc__ -> node.label
+    * haddock.modules.<catagory>.<module>.HaddockModule.__doc__ -> node.description
+* Category info
+    * haddock.modules.<catagory>.__doc__ -> category.description
+* Module DEFAULT_CONFIG -> JSON schema + UI schema:
+    * short -> description
+    * long -> $comment
+    * type=float or type=integer -> type=number
+    * type=list -> type=array
+    * type=file -> type=string + format=uri-reference + ui:widget=file
+    * type=dir -> type=string + format=uri-reference
+    * type missing -> {type:object, parameters: <key/value pairs>}
+    * min -> minimum
+    * max -> maximum
+    * minchars -> minLength
+    * maxchars -> maxLength
+    * minitems -> minItems
+    * maxitems -> maxItems
+    * accept -> ui:options={accept}
+    * choices -> enum
+    * explevel -> each explevel gets generated into own catalog
 
 TODO move script outside workflow-builder repo as this repo should be generic and not have any haddock specific scripts
 """
 
 import argparse
 import importlib
+from math import isnan
 import sys
 from yaml import dump, load, Loader
 
 from haddock.modules import modules_category
-
-LEVELS = ('basic', 'intermediate', 'guru')
+from haddock import config_expert_levels
 
 def argparser_builder():
     parser = argparse.ArgumentParser()
     parser.add_argument('out', type=argparse.FileType('w', encoding='UTF-8'), default='-')
-    parser.add_argument('--level', choices=LEVELS, default=LEVELS[0])
+    parser.add_argument('--level', choices=config_expert_levels, default=config_expert_levels[0])
     return parser
 
 def config2schema(config):
@@ -42,24 +69,23 @@ def config2schema(config):
         else:
             # If there is no default then user must supply a value so prop is required
             required.append(k)
-        if 'hover' in v and v['hover'] != 'No help yet':
-            prop['title'] = v['hover']
-        if 'doc' in v and v['doc'] != 'No help yet':
-            prop['description'] = v['doc']
+        if 'title' in v and v['title'] != 'No title yet':
+            prop['title'] = v['title']
+        if 'short' in v and v['short'] != 'No short description yet':
+            prop['description'] = v['short']
+        if 'long' in v and v['long'] != 'No long description yet':
+            prop['$comment'] = v['long']
         if 'type' not in v:
-            # TODO haddock3 file needs to be refactored so input is list of dict
-            # for now use value of mol1 key as items schema
-            schema_uiSchema = config2schema(next(iter(v.values())))
-            prop = {
-                "type": "array",
-                "items": schema_uiSchema['schema'],
-                "maxItems": len(v)
-            }
+            # if not type field treat value as dict of dicts
+            schema_uiSchema = config2schema({k2:v2 for k2,v2 in v.items() if k2 != 'explevel'})
+            prop = schema_uiSchema['schema']
             if schema_uiSchema['uiSchema']:
                 prop_ui = {
-                    "items": schema_uiSchema['uiSchema']
+                    k: schema_uiSchema['uiSchema']
                 }
-        elif v['type'] == 'bool':
+            # molXX in topaa are not required
+            required.pop()
+        elif v['type'] == 'boolean':
             prop['type'] = "boolean"
         elif v['type'] in {'float', 'integer'}:
             prop['type'] = "number"
@@ -67,37 +93,54 @@ def config2schema(config):
                 prop['maximum'] = v['max']
             if 'min' in v:
                 prop['minimum'] = v['min']
-        elif v['type'] == 'path':
+            if 'default' in v and isnan(v['default']):
+                # TODO handle nan's more gracefully
+                del prop['default']
+        elif v['type'] == 'file':
             prop['type'] = 'string'
             prop['format'] = 'uri-reference'
-            if 'length' in v:
-                # TODO rjsf gives `should NOT be longer than 9999 characters` error when maxLenght is set
-                pass
             if 'default' in v and v['default'] == '':
                 # paths can not have defaults
                 del prop['default']
 
-            # TODO move data-url to uiSchema,
-            # as workflow.cfg file use paths instead of bas64 encoded string
-            # rjsf needs data-url to render a file upload field in the form, but that can also be configured in uiSchema
+            # rjsf needs to render a file upload field which can be configured in uiSchema
             prop_ui ={
                 "ui:widget": "file"
             }
 
-            # TODO add accept key/value pair to uiSchema
             if 'accept' in v:
                 prop_ui["ui:options"] = { "accept": v['accept']}
+        elif v['type'] == 'dir':
+            prop['type'] = 'string'
+            prop['format'] = 'uri-reference'
+            if 'default' in v and v['default'] == '':
+                # paths can not have defaults
+                del prop['default']
         elif v['type'] == 'string':
             prop['type'] = 'string'
-            if 'length' in v:
-                prop['maxLength'] = v['length']
+            if 'minchars' in v:
+                prop['minLength'] = v['minchars']
+            if 'maxchars' in v:
+                prop['maxLength'] = v['maxchars']
+            if 'choices' in v:
+                prop['enum'] = v['choices']
         elif v['type'] == 'list':
             prop['type'] = "array"
-            if 'min' in v:
-                prop['minItems'] = v['min']
-            if 'max' in v:
-                prop['maxItems'] = v['max']
-            if 'items' in v:
+            if 'minitems' in v:
+                prop['minItems'] = v['minitems']
+            if 'maxitems' in v:
+                prop['maxItems'] = v['maxitems']
+            if 'itemtype' in v:
+                obj = {'a' : {'type': v['itemtype']}} # config2schema requires object
+                if 'accept' in v:
+                    obj['a']['accept'] = v['accept']
+                schema_uiSchema = config2schema(obj)
+                prop['items'] = schema_uiSchema['schema']['properties']['a']
+                if schema_uiSchema['uiSchema'] and schema_uiSchema['uiSchema']['a']:
+                    prop_ui = {
+                        "items": schema_uiSchema['uiSchema']['a']
+                    }
+            elif 'items' in v:
                 obj = {'a' : v['items']} # config2schema requires object
                 schema_uiSchema = config2schema(obj)
                 prop['items'] = schema_uiSchema['schema']['properties']['a']
@@ -111,12 +154,16 @@ def config2schema(config):
                     "type": "number"
                 }
             else:
-                raise ValueError(f"Don't know how to determine type of items of {v}")
-        # elif isinstance(v, dict):
-        #     prop = config2schema(v)
+                # TODO dont fallback to number, for example seletopclusts:top_cluster
+                prop['items'] = {
+                    "type": "number"
+                }
+                # raise ValueError(f"Don't know how to determine type of items of {v}")
         else:
             raise ValueError(f"Don't know what to do with {k}:{v}")
         properties[k] = prop
+        if 'group' in v and v['group'] != '':
+            prop_ui['ui:group'] = v['group']
         if prop_ui:
             uiSchema[k] = prop_ui
     schema = {
@@ -129,6 +176,15 @@ def config2schema(config):
         "uiSchema": uiSchema
     }
 
+def filter_on_level(config, level):
+    # Each higher level should include parameters from previous level
+    valid_levels = set()
+    for l in config_expert_levels:
+        valid_levels.add(l)
+        if l == level:
+            break
+    return {k: v for k, v in config.items() if v['explevel'] in valid_levels}
+
 def process_module(module_name, category, level):
     package = f'haddock.modules.{category}.{module_name}'
     module = importlib.import_module(package)
@@ -136,13 +192,7 @@ def process_module(module_name, category, level):
     with open(module.DEFAULT_CONFIG) as f:
         config = load(f, Loader=Loader)
 
-    # Each higher level should include parameters from previous level
-    config4level = {}
-    for l in LEVELS:
-        config4level.update(config[l])
-        if l == level:
-            break
-
+    config4level = filter_on_level(config, level)
     schema_uiSchema = config2schema(config4level)
     # TODO add $schema and $id to schema
     return {
@@ -162,30 +212,41 @@ def process_category(category):
         'description': module.__doc__,
     }
 
-# TODO retrieve global config in haddock3 code
-# TODO in haddock3 this section is called general run parameters, we could rename global to general
-GLOBAL_CONFIG = load("""
-molecules:
-    type: list
-    items:
-        type: path
-        accept: '.pdb'
-    min: 2
-    max: 10
-    hover: Molecules
-    doc: No help yet
-run_dir:
-    type: string
-    hover: Run directory
-    doc: No help yet
-ncores:
-    type: integer
-    hover: Number of cpu cores
-    doc: No help yet
-    default: 8
-""", Loader=Loader)
+def process_global(level):
+    package = 'haddock.modules'
+    module = importlib.import_module(package)
+    with open(module.modules_defaults_path) as f:
+        optional_global_parameters = load(f, Loader=Loader)
+    config = REQUIRED_GLOBAL_PARAMETERS | optional_global_parameters
+    config4level = filter_on_level(config, level)
 
-GLOBAL_NODE = config2schema(GLOBAL_CONFIG)
+    schema_uiSchema = config2schema(config4level)
+    # TODO add $schema and $id to schema
+    return {
+        "schema": schema_uiSchema['schema'],
+        "uiSchema": schema_uiSchema['uiSchema']
+    }
+
+
+# TODO retrieve required global config from haddock3 code
+REQUIRED_GLOBAL_PARAMETERS = {
+    'molecules': {
+        'type': 'list',
+        'itemtype': 'file',
+        'accept': '.pdb',
+        'minitems': 1,
+        'maxitems': 20,
+        'title': 'Molecules',
+        'group': '',
+        'explevel': 'basic'
+    },
+    'run_dir': {
+        'type': 'dir',
+        'title': 'Run directory',
+        'group': '',
+        'explevel': 'basic'
+    }
+}
 
 def main(argv=sys.argv[1:]):
     argparser = argparser_builder()
@@ -194,20 +255,22 @@ def main(argv=sys.argv[1:]):
     # TODO order the categories by which category needs output from another. Now order is not reproducible
     categories = [process_category(c) for c in set(modules_category.values())]
 
-    broken_modules = {'clustfcc', 'topocg'}
+    broken_modules = {
+        'clustfcc',  # Gives `ModuleNotFoundError: No module named 'fcc.scripts'`` error
+        'topocg', # Gives `AttributeError: module 'haddock.modules.topology.topocg' has no attribute 'HaddockModule'` error
+    }
     nodes = [process_module(module, category, args.level) for module, category in modules_category.items() if module not in broken_modules]
 
     catalog = {
         "title": f"Haddock 3 {args.level}",
         "categories": categories,
-        "global": GLOBAL_NODE,
-        # TODO in haddock3 nodes are called modules, we could rename it here
+        'global': process_global(args.level),
         "nodes": nodes,
         "examples": {
             'docking': '/examples/docking-protein-ligand.zip' # TODO get from somewhere instead of hardcoding it here
         }
     }
-    dump(catalog, args.out)
+    dump(catalog, args.out, sort_keys=False)
 
 if __name__ == '__main__':
     sys.exit(main())
