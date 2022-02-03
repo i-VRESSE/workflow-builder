8ff0ffac0f765e5347155ee062248fbc01825b03
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-02-03 12:12:53 +0100

    Adapt to new expertise levels + Use catalog/index.json instead of hardcoded list of catalog urls

--- src/constants.tsx
+++ src/constants.tsx
@@ -3,8 +3,4 @@ export const workflowFilename = 'workflow.cfg'
 
 export const workflowArchiveFilename = 'workflow.zip'
 
-export const catalogURLchoices = [
-  ['haddock3basic', new URL('/haddock3.basic.catalog.yaml', import.meta.url).href],
-  ['haddock3intermediate', new URL('/haddock3.intermediate.catalog.yaml', import.meta.url).href],
-  ['haddock3guru', new URL('/haddock3.guru.catalog.yaml', import.meta.url).href]
-]
+export const catalogIndexURL = new URL('/catalog/index.json', import.meta.url).href
