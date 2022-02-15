5d8b0bf5cf4fc866112efd6f97477e676506881c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-02-15 15:17:34 +0100

    Add grouper methods to render form with grouped props
    Refs #25

---
+++ src/grouper.ts
@@ -0,0 +1,93 @@
+import { UiSchema } from '@rjsf/core'
+import { IParameters } from './types'
+import { JSONSchema7 } from 'json-schema'
+import { isObject } from './utils/isObject'
+
+// TODO call when loading catalog and store in catalog.node.grouped...
+export function groupSchema (schema: JSONSchema7, uiSchema: UiSchema): JSONSchema7 {
+  const newSchema = JSON.parse(JSON.stringify(schema))
+
+  Object.entries(uiSchema).forEach(([k, v]) => {
+    // TODO recursivly, now only loops over first direct props
+    if ('ui:group' in v) {
+      const group = v['ui:group']
+      if (!(group in newSchema.properties)) {
+        newSchema.properties[group] = {
+          type: 'object',
+          properties: {},
+          additionalProperties: false
+        }
+      }
+      newSchema.properties[group].properties[k] = newSchema.properties[k]
+      /* eslint-disable @typescript-eslint/no-dynamic-delete */
+      delete newSchema.properties[k]
+      /* eslint-enable @typescript-eslint/no-dynamic-delete */
+    }
+  })
+
+  return newSchema
+}
+
+export function groupUiSchema (uiSchema: UiSchema): UiSchema {
+  const newUiSchema: UiSchema = {}
+  Object.entries(uiSchema).forEach(([k, v]) => {
+    // TODO recursivly, now only loops over first direct props
+    if ('ui:group' in v) {
+      const group = v['ui:group']
+      const newuiProp = { ...v } // Shallow copy
+      /* eslint-disable @typescript-eslint/no-dynamic-delete */
+      delete newuiProp['ui:group']
+      /* eslint-enable @typescript-eslint/no-dynamic-delete */
+      if (Object.keys(newuiProp).length > 0) {
+        if (!(group in newUiSchema)) {
+          newUiSchema[group] = {}
+        }
+        newUiSchema[group][k] = newuiProp
+      }
+    } else {
+      newUiSchema[k] = v
+    }
+  })
+  return newUiSchema
+}
+
+// TODO call when passing parameters to form
+export function groupParameters (parameters: IParameters, uiSchema: UiSchema): IParameters {
+  const newParameters: IParameters = {}
+
+  Object.entries(parameters).forEach(([k, v]) => {
+    if (k in uiSchema && 'ui:group' in uiSchema[k]) {
+      const group = uiSchema[k]['ui:group']
+      if (!(group in newParameters)) {
+        newParameters[group] = {}
+      }
+      (newParameters[group] as IParameters)[k] = v
+      /* eslint-disable @typescript-eslint/no-dynamic-delete */
+      delete newParameters[k]
+      /* eslint-enable @typescript-eslint/no-dynamic-delete */
+    } else {
+      newParameters[k] = v
+    }
+  })
+
+  return newParameters
+}
+
+// TODO call when retrieving parameters from form
+export function unGroupParameters (parameters: IParameters, uiSchema: UiSchema): IParameters {
+  const newParameters: IParameters = {}
+  Object.entries(parameters).forEach(([k, v]) => {
+    if (isObject(v)) {
+      // TODO recursivly, now only loops over first direct props
+      Object.entries(v as IParameters).forEach(([gk, gv]) => {
+        if (gk in uiSchema && 'ui:group' in uiSchema[gk]) {
+          newParameters[gk] = gv
+        }
+      })
+    } else {
+      newParameters[k] = v
+    }
+  })
+
+  return newParameters
+}
