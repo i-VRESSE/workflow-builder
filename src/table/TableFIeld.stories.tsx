6ab4467acb493c88cd4fd97d64c53326f94a1d23
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-03-14 16:25:32 +0100

    Add story for custom column width in table

--- src/table/TableFIeld.stories.tsx
+++ src/table/TableFIeld.stories.tsx
@@ -98,3 +98,52 @@ export const ArrayOfObjectsAsTable: ComponentStory<typeof Form> = () => {
   }
   return render(schema, uiSchema, formData)
 }
+
+export const ArrayOfObjectsAsTableWithCustomWidth: ComponentStory<typeof Form> = () => {
+  const schema: JSONSchema7 = {
+    type: 'object',
+    properties: {
+      nested1: {
+        type: 'array',
+        title: 'Array of {prop1,prop2}',
+        items: {
+          type: 'object',
+          properties: {
+            prop1: {
+              title: 'Prop 1',
+              description: 'Description 1',
+              type: 'string'
+            },
+            prop2: {
+              title: 'Prop 2',
+              description: 'Description 2',
+              type: 'string'
+            }
+          },
+          additionalProperties: false
+        }
+      }
+    },
+    additionalProperties: false
+  }
+  const uiSchema = {
+    nested1: {
+      'ui:field': 'table',
+      'ui:options': {
+        widths: {
+          prop1: '30%'
+        }
+      }
+    }
+  }
+  const formData = {
+    nested1: [{
+      prop1: '11',
+      prop2: '22'
+    }, {
+      prop1: '33',
+      prop2: '44'
+    }]
+  }
+  return render(schema, uiSchema, formData)
+}
