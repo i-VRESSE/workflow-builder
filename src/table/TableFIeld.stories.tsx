dbbe6d89f4e0c30186d89eaf2e123e284b680cf0
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-03-22 16:44:38 +0100

    Add story with object as cell + Hide object title and descriptions in cell

--- src/table/TableFIeld.stories.tsx
+++ src/table/TableFIeld.stories.tsx
@@ -147,3 +147,59 @@ export const ArrayOfObjectsAsTableWithCustomWidth: ComponentStory<typeof Form> =
   }
   return render(schema, uiSchema, formData)
 }
+
+export const ArrayOfObjectsOfObjectsAsTable: ComponentStory<typeof Form> = () => {
+  const schema: JSONSchema7 = {
+    type: 'object',
+    properties: {
+      nested1: {
+        type: 'array',
+        title: 'Array of {prop1,prop2}',
+        items: {
+          type: 'object',
+          properties: {
+            prop1: {
+              title: 'Prop 1',
+              description: 'Description 1',
+              type: 'object',
+              properties: {
+                prop1a: {
+                  type: 'string',
+                  title: 'Prop 1 a',
+                  description: 'Description 1 a'
+                },
+                prop1b: {
+                  type: 'string',
+                  title: 'Prop 1 b',
+                  description: 'Description 1 b'
+                }
+              }
+            },
+            prop2: {
+              title: 'Prop 2',
+              description: 'Description 2',
+              type: 'string'
+            }
+          },
+          additionalProperties: false
+        }
+      }
+    },
+    additionalProperties: false
+  }
+  const uiSchema = {
+    nested1: {
+      'ui:field': 'table'
+    }
+  }
+  const formData = {
+    nested1: [{
+      prop1: '11',
+      prop2: '22'
+    }, {
+      prop1: '33',
+      prop2: '44'
+    }]
+  }
+  return render(schema, uiSchema, formData)
+}
