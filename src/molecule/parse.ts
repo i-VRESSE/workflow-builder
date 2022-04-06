85ecfcaa777700057ff19b78679ff562c3ed036f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-04-06 14:00:15 +0200

    Added simple PDB parser

---
+++ src/molecule/parse.ts
@@ -0,0 +1,24 @@
+export interface Pdb {
+  chains: string[]
+  residueSequenceNumbers: number[]
+}
+
+export function parsePDB (content: string): Pdb {
+  // Code partially inspired by https://github.com/justinmc/parse-pdb/blob/master/index.js
+  const pdbLines = content.split('\n')
+  const chains = new Set<string>()
+  const residueSequenceNumbers = new Set<number>()
+  pdbLines
+    .filter(l => l.startsWith('ATOM  '))
+    .forEach((l) => {
+      // Use ATOM record format v3.3, see http://www.wwpdb.org/documentation/file-format-content/format33/sect9.html#ATOM
+      const chainID = l.substring(21, 22).trim()
+      chains.add(chainID)
+      const resSeq = parseInt(l.substring(22, 26))
+      residueSequenceNumbers.add(resSeq)
+    })
+  return {
+    chains: Array.from(chains),
+    residueSequenceNumbers: Array.from(residueSequenceNumbers)
+  }
+}
