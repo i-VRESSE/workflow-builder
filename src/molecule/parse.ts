5cfcfb8dffe9b1f7b7dcd0d9bce7a44468a3fb5e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-05-09 14:34:43 +0200

    Use pdbtbx-ts from npmjs.com

--- src/molecule/parse.ts
+++ src/molecule/parse.ts
@@ -1,4 +1,4 @@
-import pdbtbx, { open_pdb } from 'pdbtbx-ts'
+import pdbtbx, { open_pdb } from '@i-vresse/pdbtbx-ts'
 
 export interface MoleculeInfo {
   chains: string[]
@@ -38,16 +38,17 @@ export async function parsePDB2 (content: string): Promise<MoleculeInfo> {
   if (process.env.NODE_ENV === 'test') {
     // vitest is run in NodeJS so needs wasm file read from disk instead of fetch using url
     const { readFile } = await import('fs/promises')
-    const wasmBuffer = await readFile('node_modules/pdbtbx-ts/pdbtbx_ts_bg.wasm')
+    const wasmBuffer = await readFile('node_modules/@i-vresse/pdbtbx-ts/pdbtbx_ts_bg.wasm')
     await pdbtbx(wasmBuffer)
   } else {
     // To make vite aware of wasm file, it needs to passed to pdbtbx-ts default method.
     // TODO make prettier URL
-    const mod = new URL('../../node_modules/pdbtbx-ts/pdbtbx_ts_bg.wasm', import.meta.url)
+    const mod = new URL('../../node_modules/@i-vresse/pdbtbx-ts/pdbtbx_ts_bg.wasm', import.meta.url)
     await pdbtbx(mod)
   }
   try {
     const info = open_pdb(content)
+    console.info(info.warnings)
     return {
       chains: info.chains,
       residueSequenceNumbers: info.residue_sequence_numbers
