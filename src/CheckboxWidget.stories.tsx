8231d84aedadf84fb91f1c09278b330c6fec2e59
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-05-17 10:40:38 +0200

    Make separate section for checkbox widget and clean up code

--- src/CheckboxWidget.stories.tsx
+++ src/CheckboxWidget.stories.tsx
@@ -1,52 +1,43 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { CollapsibleField } from './CollapsibleField'
 import { action } from '@storybook/addon-actions'
 import { Form } from './Form'
 import { JSONSchema7 } from 'json-schema'
-import { UiSchema } from '@rjsf/core'
+import { Widgets } from '@rjsf/bootstrap-4/'
 
 
-const meta: ComponentMeta<typeof CollapsibleField> = {
-    title: 'CollapsibleField',
-    component: CollapsibleField
-  }
+const meta: ComponentMeta<typeof Widgets.CheckboxWidget> = {
+  title: 'Checkbox',
+  component: Widgets.CheckboxWidget
+}
 
-  export default meta
+export default meta
 
-  function render (schema: JSONSchema7, uiSchema: UiSchema): JSX.Element {
-    const fields = {
-      collapsible: CollapsibleField
-    }
-    return (
-      <Form
-        fields={fields}
-        schema={schema}
-        uiSchema={uiSchema}
-        onSubmit={action('onSubmit')}
-      />
-    )
-  }
+function render(schema: JSONSchema7): JSX.Element {
+  debugger
+  return (
+    <Form
+      schema={schema}
+      onSubmit={action('onSubmit')}
+    />
+  )
+}
 
 
 export const CheckboxWidget: ComponentStory<typeof Form> = () => {
-    const schema: JSONSchema7 = {
-        "type": "object",
-        "title": "Boolean field",
-        "properties": {
-            "default": {
-            "type": "boolean",
-            "title": "checkbox (default)",
-            "description": "This is the checkbox-description"
-            }
-        },
-      additionalProperties: false
-    }
-    const uiSchema = {
-      group1: {
-        'ui:field': 'collapsible'
+  const schema: JSONSchema7 = {
+    "type": "object",
+    "title": "Boolean field",
+    "properties": {
+      "default": {
+        "type": "boolean",
+        "title": "checkbox (default)",
+        "description": "This is the checkbox-description"
       }
-    }
-    return render(schema, uiSchema)
+    },
+    additionalProperties: false
   }
+
+  return render(schema)
+}
 
 
