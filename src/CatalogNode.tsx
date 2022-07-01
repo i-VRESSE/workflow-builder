1415100989a0e00c8c5b4cbe4788caf2f716dff2
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2022-07-01 12:05:16 +0200

    Some styling

--- src/CatalogNode.tsx
+++ src/CatalogNode.tsx
@@ -22,11 +22,11 @@ export const CatalogNode = ({ id, label }: ICatalogNode): JSX.Element => {
         className='btn btn-light btn-sm'
         onClick={() => addNodeToWorkflow(id)}
       >
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <span>
             {id}
           </span>
-          <div ref={setActivatorNodeRef} {...listeners} className='btn btn-light btn-sm' title='Move'>
+          <div ref={setActivatorNodeRef} {...listeners} className='btn btn-light btn-sm' title='Move' style={{cursor: 'grab'}}>
             <GripVertical />
           </div>
         </div>
--- src/VisualNode.tsx
+++ src/VisualNode.tsx
@@ -1,19 +1,19 @@
-import { useSelectNodeIndex, useWorkflow } from './store'
-import { GripVertical, X } from 'react-bootstrap-icons'
-import { CSS } from '@dnd-kit/utilities'
-import { nodeWidth } from './constants'
-import { useSortable } from '@dnd-kit/sortable'
+import { useSelectNodeIndex, useWorkflow } from "./store";
+import { GripVertical, X } from "react-bootstrap-icons";
+import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
+import classes from "./VisualNode.module.css";
 
 interface IProp {
-  id: string
-  index: number
-  code: string
+  id: string;
+  index: number;
+  code: string;
 }
 
 export const VisualNode = ({ id, index, code }: IProp): JSX.Element => {
   // TODO to power hover use css :hover instead of slower JS
-  const selectedNodeIndex = useSelectNodeIndex()
-  const { selectNode, deleteNode } = useWorkflow()
+  const selectedNodeIndex = useSelectNodeIndex();
+  const { selectNode, deleteNode } = useWorkflow();
 
   const {
     attributes,
@@ -21,42 +21,48 @@ export const VisualNode = ({ id, index, code }: IProp): JSX.Element => {
     setNodeRef,
     setActivatorNodeRef,
     transform,
-    transition
-  } = useSortable({ id: code })
+    transition,
+  } = useSortable({ id: code });
 
   const style = {
     transform: CSS.Transform.toString(transform),
-    transition
-  }
-  const selectedStyle = selectedNodeIndex === index ? { fontWeight: 'bold' } : {}
+    transition,
+  };
+  const selectedStyle =
+    selectedNodeIndex === index ? { fontWeight: "bold" } : {};
 
   // TODO after clicking node the active styling is not removed unless you activate another element
   return (
     <div ref={setNodeRef} style={style} {...attributes}>
-      <div
-        className='btn-group'
+      <button
+        className={"btn btn-light btn-sm btn-block " + classes.node}
+        title="Click to configure"
+        style={selectedStyle}
+        onClick={() => {
+          selectNode(index);
+        }}
       >
-        <button
-          className='btn btn-light btn-sm btn-block'
-          title='Click to configure'
-          style={{ width: `${nodeWidth}rem`, display: 'flex', justifyContent: 'space-between', ...selectedStyle }}
-          onClick={() => {
-            selectNode(index)
-          }}
-        >
+        <span>
           {index + 1}. {id}
-        </button>
-        <button ref={setActivatorNodeRef} {...listeners} className='btn btn-light btn-sm' title='Move'>
+        </span>
+        <div>
+        <div
+          ref={setActivatorNodeRef}
+          {...listeners}
+          className={"btn btn-light btn-sm " + classes.grip}
+          title="Move"
+        >
           <GripVertical />
-        </button>
-        <button
-          title='Delete'
-          className='btn btn-light btn-sm'
+        </div>
+        <div
+          title="Delete"
+          className={"btn btn-light btn-sm " + classes.delete}
           onClick={() => deleteNode(index)}
         >
           <X />
-        </button>
-      </div>
+        </div>
+        </div>
+      </button>
     </div>
-  )
-}
+  );
+};
