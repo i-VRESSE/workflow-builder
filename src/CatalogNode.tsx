fdc39a3a7ef5b8c4f205fa6ed31832493f21551f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2022-07-01 12:41:14 +0200

    More styling

--- src/CatalogNode.tsx
+++ src/CatalogNode.tsx
@@ -1,37 +1,44 @@
-import { nodeWidth } from './constants'
-import { useWorkflow } from './store'
-import { ICatalogNode } from './types'
-import { GripVertical } from 'react-bootstrap-icons'
-import { useDraggable } from '@dnd-kit/core'
+import { GripVertical } from "react-bootstrap-icons";
+import { useDraggable } from "@dnd-kit/core";
+
+import { nodeWidth } from "./constants";
+import { useWorkflow } from "./store";
+import { ICatalogNode } from "./types";
+import classes from "./CatalogNode.module.css";
 
 export const CatalogNode = ({ id, label }: ICatalogNode): JSX.Element => {
-  const { attributes, listeners, setNodeRef, transform, setActivatorNodeRef } = useDraggable({ id, data: { catalog: true } })
-  const style = (transform != null)
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
-      }
-    : undefined
+  const { attributes, listeners, setNodeRef, transform, setActivatorNodeRef } =
+    useDraggable({ id, data: { catalog: true } });
+  const style =
+    transform != null
+      ? {
+          transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+        }
+      : undefined;
 
-  const { addNodeToWorkflow } = useWorkflow()
+  const { addNodeToWorkflow } = useWorkflow();
 
   return (
     <li>
       <button
-        ref={setNodeRef} style={{ ...style, width: `${nodeWidth}rem` }} {...attributes}
+        ref={setNodeRef}
+        style={{ ...style, width: `${nodeWidth}rem` }}
+        {...attributes}
         title={label}
-        className='btn btn-light btn-sm'
+        className={"btn btn-light btn-sm " + classes.node}
         onClick={() => addNodeToWorkflow(id)}
       >
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <span>
-            {id}
-          </span>
-          <div ref={setActivatorNodeRef} {...listeners} className='btn btn-light btn-sm' title='Move' style={{cursor: 'grab'}}>
-            <GripVertical />
-          </div>
+        <span>{id}</span>
+        <div
+          ref={setActivatorNodeRef}
+          {...listeners}
+          className={"btn btn-light btn-sm " + classes.grip}
+          title="Move"
+          style={{ cursor: "grab" }}
+        >
+          <GripVertical />
         </div>
       </button>
-
     </li>
-  )
-}
+  );
+};
---
+++ src/modules.d.ts
@@ -0,0 +1,4 @@
+declare module "*.module.css" {
+    const styles: any;
+    export default styles;
+  }
