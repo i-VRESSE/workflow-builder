82b21d0a2f42cd8425a16b7cf3dba5317cf7bf6f
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-07-05 15:40:27 +0200

    fix linter issue

--- integration-tests/syntaxhighlighter.spec.ts
+++ integration-tests/syntaxhighlighter.spec.ts
@@ -4,15 +4,14 @@ test('Verify that [object Object] bug does not show up (issue #74)', async ({ pa
   await page.goto('http://localhost:3000')
 
   // Load the example
-  await page.locator('text=docking-protein-ligand').click();
+  await page.locator('text=docking-protein-ligand').click()
 
   // Click 'text' and ensure the files appear
-  await page.locator('text=Text').click();
-  await page.waitForSelector('button:has-text("data/target.pdb")');
+  await page.locator('text=Text').click()
+  await page.waitForSelector('button:has-text("data/target.pdb")')
 
   // verify that the [object Object] text does not show up
   // see https://github.com/i-VRESSE/workflow-builder/issues/74
-  const highlightedCode = await page.locator('#highlightedcode pre');
+  const highlightedCode = await page.locator('#highlightedcode pre')
   await expect(highlightedCode).not.toContainText('[object Object]')
-
 })
