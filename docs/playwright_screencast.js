4a7aede70d3e2d07a4b69686506087921f260e7a
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-07-08 16:42:11 +0200

    fix linter issues

--- docs/playwright_screencast.js
+++ docs/playwright_screencast.js
@@ -6,23 +6,23 @@ const { chromium } = require('playwright');
   // open browser and page
   const browser = await chromium.launch({
     headless: false
-  });
-  const context = await browser.newContext({ recordVideo: { dir: 'docs/videos/' } });
-  const page = await context.newPage();
+  })
+  const context = await browser.newContext({ recordVideo: { dir: 'docs/videos/' } })
+  const page = await context.newPage()
 
   // await page.goto('https://wonderful-noether-53a9e8.netlify.app/');
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:3000/')
 
   // workflow example steps
-  await page.locator('text=docking-protein-ligand').click();
-  await page.locator('button:has-text("2. rigidbody")').click();
-  await page.locator('#expander4sampling svg').click();
-  await page.locator('input[type="number"]').click();
-  await page.locator('input[type="number"]').fill('2');
-  await page.locator('text=Submit').click();
-  await page.locator('text=Text').click();
+  await page.locator('text=docking-protein-ligand').click()
+  await page.locator('button:has-text("2. rigidbody")').click()
+  await page.locator('#expander4sampling svg').click()
+  await page.locator('input[type="number"]').click()
+  await page.locator('input[type="number"]').fill('2')
+  await page.locator('text=Submit').click()
+  await page.locator('text=Text').click()
 
   // close browser
-  await context.close();
-  await browser.close();
-})();
+  await context.close()
+  await browser.close()
+})()
