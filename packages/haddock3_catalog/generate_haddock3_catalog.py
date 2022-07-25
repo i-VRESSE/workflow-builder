c9feaea0a443e784c739934896e53f100a3776e4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-07-25 11:19:48 +0200

    Add ui:indexable to parameters with maxItemsFrom:molecules + add global preprocess parameter

--- packages/haddock3_catalog/generate_haddock3_catalog.py
+++ packages/haddock3_catalog/generate_haddock3_catalog.py
@@ -138,6 +138,7 @@ def config2schema(config):
                 'items': schemas['schema'],
                 'maxItemsFrom': 'molecules',
             })
+            prop_ui['ui:indexable'] = True
             # TODO rjsf gives error when description is present, so for now remove it
             del prop['description']
 
@@ -239,6 +240,7 @@ def config2schema(config):
                 prop['maxItems'] = v['maxitems']
             elif 'maxItemsFrom' in v:
                 prop['maxItemsFrom'] = v['maxItemsFrom']
+                prop_ui['ui:indexable'] = True
             if 'properties' in v:
                 obj_schemas = config2schema(v['properties'])
                 if v['dim'] == 1:
@@ -396,11 +398,13 @@ def process_global(level):
     package = 'haddock.modules'
     module = importlib.import_module(package)
     with open(module.modules_defaults_path) as f:
-        optional_global_parameters = load(f, Loader=Loader)
+        modules_defaults = load(f, Loader=Loader)
     gmodule = importlib.import_module('haddock.gear.parameters')
     with open(gmodule.MANDATORY_YAML) as f:
         mandatory_parameters = load(f, Loader=Loader)
-    config = mandatory_parameters | optional_global_parameters
+    with open(gmodule.OPTIONAL_YAML) as f:
+        optional_parameters = load(f, Loader=Loader)
+    config = mandatory_parameters | optional_parameters | modules_defaults
     config4level = filter_on_level(config, level)
 
     schemas = config2schema(config4level)
