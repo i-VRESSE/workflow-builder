8461419e02bd509d024d473dfabb7c0d0e933b5d
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-03 11:16:14 +0200

    Add store test + Upgrade to nodejs 18 + drop unused @storybook/addon-links
    The store test loads the zip library which required TransformStream to be defined.
    TransformStreams is not defined in node 16, but it is in node 18 so perform node upgrade.
    
    To fix storybook for node 18 a workaround `--openssl-legacy-provider` was added to the scripts

--- packages/core/.storybook/main.cjs
+++ packages/core/.storybook/main.cjs
@@ -1,12 +1,8 @@
-const { mergeConfig } = require('vite');
+const { mergeConfig } = require("vite");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   core: {
     builder: "@storybook/builder-vite",
   },
@@ -18,30 +14,19 @@ module.exports = {
       // of which es2019 and safari31.1 are not compatible with bigint according to
       // https://github.com/evanw/esbuild/blob/0d4fae1f97b2013b78a293f1f19607df062a52f1/internal/compat/js_table.go#L262-L270
       // bigints are used in @ltd/j-toml as its a dependency we only have to change the target of the deps
-      // So overwrite target with target which are bigint compatible 
+      // So overwrite target with target which are bigint compatible
       // For `yarn build-storybook` overwrite build target
       build: {
-        target: [
-              "es2020",
-              "edge88",
-              "firefox78",
-              "chrome87",
-              "safari14",
-            ],
+        target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+        sourcemap: false
       },
       // For `yarn storybook` overwrite build target for dependencies
       optimizeDeps: {
         esbuildOptions: {
-          target: [
-            "es2020",
-            "edge88",
-            "firefox78",
-            "chrome87",
-            "safari14",
-          ]
-        }
+          target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+        },
       },
-      base: ''
+      base: "", // Make storybook-static work in non / path
     });
   },
 };
