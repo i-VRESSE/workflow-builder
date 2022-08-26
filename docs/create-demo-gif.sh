5c431d3997cad9adf235357089ff8edb286e62cd
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-08-26 12:11:46 +0200

    update demo gif

--- docs/create-demo-gif.sh
+++ docs/create-demo-gif.sh
@@ -3,8 +3,8 @@ mkdir docs/temp
 # run playwright
 node docs/playwright_screencast.js
 
-# get video
-ffmpeg -i docs/temp/*.webm -r 10 -f image2 docs/temp/frame-%04d.png
+# create high quality GIF with gif.ski
+ffmpeg -i docs/temp/*.webm -f image2 docs/temp/frame-%04d.png
 gifski -o docs/demo.gif docs/temp/frame*.png --width 1920
 
 rm -r docs/temp
